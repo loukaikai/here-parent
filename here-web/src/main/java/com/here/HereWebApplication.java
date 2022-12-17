1155ed809cad2d182fb0f94382e3f5f539543dc3
szp <2077084603@qq.com>
2022-12-17 16:05:47 +0800

    登录

--- here-web/src/main/java/com/here/HereWebApplication.java
+++ here-web/src/main/java/com/here/HereWebApplication.java
@@ -2,6 +2,8 @@ package com.here;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class HereWebApplication {
@@ -10,4 +12,8 @@ public class HereWebApplication {
         SpringApplication.run(HereWebApplication.class, args);
     }
 
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
