7cf92a290cd92acc1af48d2650d356b0583ddbd5
szp <2077084603@qq.com>
2022-12-17 14:44:09 +0800

    web构建

--- here-common/src/main/java/com/here/common/utils/WechatUtil.java
+++ here-common/src/main/java/com/here/common/utils/WechatUtil.java
@@ -9,18 +9,18 @@ import java.util.Map;
 
 public class WechatUtil {
     @Value("${wechart.appId}")
-    private String appId;
+    private static String appId;
 
     @Value("${wechart.secret}")
-    private String secret;
+    private static String secret;
 
     //https://mp.weixin.qq.com/wxopen/devprofile?action=get_profile&token=164113089&lang=zh_CN
     public static JSONObject getSessionKeyOrOpenId(String code) {
         String requestUrl = "https://api.weixin.qq.com/sns/jscode2session";
         Map<String, String> requestUrlParam = new HashMap<>();
 
-        requestUrlParam.put("appid", "");
-        requestUrlParam.put("secret", "");
+        requestUrlParam.put("appid", appId);
+        requestUrlParam.put("secret", secret);
         requestUrlParam.put("js_code", code);
         //默认参数
         requestUrlParam.put("grant_type", "authorization_code");
