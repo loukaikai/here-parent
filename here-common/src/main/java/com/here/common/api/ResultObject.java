de508514b9be7a0275703748a31c4b494575cbd0
loukaikai <1650918851@qq.com>
2022-12-17 16:28:44 +0800

    将int 改为long

--- here-common/src/main/java/com/here/common/api/ResultObject.java
+++ here-common/src/main/java/com/here/common/api/ResultObject.java
@@ -8,10 +8,97 @@ import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
-public class ResultObject implements Serializable {
+public class ResultObject<T> implements Serializable {
 
     private Object data;
     private String message;
-    private Integer code;
+    private long code;
+
+    protected ResultObject(long code, String message, T data) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+    }
+
+    /**
+     * 成功返回结果
+     *
+     * @param data 获取的数据
+     */
+    public static <T> ResultObject<T> success(T data) {
+        return new ResultObject<T>(ResultCode.SUCCESS.getCode(), ResultCode.SUCCESS.getMessage(), data);
+    }
+
+    /**
+     * 成功返回结果
+     *
+     * @param data 获取的数据
+     * @param  message 提示信息
+     */
+    public static <T> ResultObject<T> success(T data, String message) {
+        return new ResultObject<T>(ResultCode.SUCCESS.getCode(), message, data);
+    }
+
+    /**
+     * 失败返回结果
+     * @param errorCode 错误码
+     */
+    public static <T> ResultObject<T> failed(IErrorCode errorCode) {
+        return new ResultObject<T>(errorCode.getCode(), errorCode.getMessage(), null);
+    }
+
+    /**
+     * 失败返回结果
+     * @param errorCode 错误码
+     * @param message 错误信息
+     */
+    public static <T> ResultObject<T> failed(IErrorCode errorCode,String message) {
+        return new ResultObject<T>(errorCode.getCode(), message, null);
+    }
+
+    /**
+     * 失败返回结果
+     * @param message 提示信息
+     */
+    public static <T> ResultObject<T> failed(String message) {
+        return new ResultObject<T>(ResultCode.FAILED.getCode(), message, null);
+    }
+
+    /**
+     * 失败返回结果
+     */
+    public static <T> ResultObject<T> failed() {
+        return failed(ResultCode.FAILED);
+    }
+
+    /**
+     * 参数验证失败返回结果
+     */
+    public static <T> ResultObject<T> validateFailed() {
+        return failed(ResultCode.VALIDATE_FAILED);
+    }
+
+    /**
+     * 参数验证失败返回结果
+     * @param message 提示信息
+     */
+    public static <T> ResultObject<T> validateFailed(String message) {
+        return new ResultObject<T>(ResultCode.VALIDATE_FAILED.getCode(), message, null);
+    }
+
+    /**
+     * 未登录返回结果
+     */
+    public static <T> ResultObject<T> unauthorized(T data) {
+        return new ResultObject<T>(ResultCode.UNAUTHORIZED.getCode(), ResultCode.UNAUTHORIZED.getMessage(), data);
+    }
+
+    /**
+     * 未授权返回结果
+     */
+    public static <T> ResultObject<T> forbidden(T data) {
+        return new ResultObject<T>(ResultCode.FORBIDDEN.getCode(), ResultCode.FORBIDDEN.getMessage(), data);
+    }
+
 }
 
