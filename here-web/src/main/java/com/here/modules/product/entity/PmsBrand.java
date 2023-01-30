79b7d02f6af651433e930378fc4263a828661532
banjiu00 <527134765@qq.com>
2023-01-30 19:41:43 +0800

    添加商品相关entity

---
+++ here-web/src/main/java/com/here/modules/product/entity/PmsBrand.java
@@ -0,0 +1,50 @@
+package com.here.modules.product.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 品牌表
+ *
+ * @author syj
+ * @since 2023-01-19
+ */
+@Data
+@TableName("pms_brand")
+@ApiModel(value = "PmsBrand", description = "品牌表")
+public class PmsBrand implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @ApiModelProperty("品牌名")
+    private String name;
+
+    @ApiModelProperty("检索首字母")
+    private String firstLetter;
+
+    @ApiModelProperty("品牌logo地址")
+    private String logo;
+
+    @ApiModelProperty("品牌介绍")
+    private String description;
+
+    @ApiModelProperty("显示状态[0-不显示；1-显示]")
+    private Integer showStatus;
+
+    @ApiModelProperty("排序")
+    private Integer sort;
+
+    @ApiModelProperty("创建时间")
+    private Date createTime;
+
+}
