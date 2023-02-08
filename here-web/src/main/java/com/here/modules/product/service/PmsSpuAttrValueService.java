0fc2d00a175484c3e26ed4919f18c042638b2b22
banjiu00 <527134765@qq.com>
2023-02-08 20:53:54 +0800

    修改spu属性值管理

--- here-web/src/main/java/com/here/modules/product/service/PmsSpuAttrValueService.java
+++ here-web/src/main/java/com/here/modules/product/service/PmsSpuAttrValueService.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.here.modules.product.entity.PmsSpuAttrValue;
 
+import java.util.List;
+
 /**
  * 商品品牌管理Service
  *
@@ -15,4 +17,12 @@ public interface PmsSpuAttrValueService extends IService<PmsSpuAttrValue> {
      * 分页查询spu属性值
      */
     Page<PmsSpuAttrValue> list(String keyword, int pageNum, int pageSize);
+
+    void saveProductAttr(List<PmsSpuAttrValue> collect);
+
+
+    List<PmsSpuAttrValue> baseAttrlistforspu(Long spuId);
+
+
+    void updateSpuAttr(Long spuId, List<PmsSpuAttrValue> entities);
 }
--- here-web/src/main/java/com/here/modules/product/service/impl/PmsSpuAttrValueServiceImpl.java
+++ here-web/src/main/java/com/here/modules/product/service/impl/PmsSpuAttrValueServiceImpl.java
@@ -10,6 +10,9 @@ import com.here.modules.product.mapper.PmsSpuAttrValueMapper;
 import com.here.modules.product.service.PmsSpuAttrValueService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 商品品牌管理Service实现类
  *
@@ -27,6 +30,24 @@ public class PmsSpuAttrValueServiceImpl extends ServiceImpl<PmsSpuAttrValueMappe
             lambda.eq(PmsSpuAttrValue::getAttrName, keyword);
         }
         return page(page, wrapper);
+    }
+
+    @Override
+    public void saveProductAttr(List<PmsSpuAttrValue> collect) {
+        this.saveBatch(collect);
+    }
+
+    @Override
+    public List<PmsSpuAttrValue> baseAttrlistforspu(Long spuId) {
+        return this.baseMapper.selectList(new QueryWrapper<PmsSpuAttrValue>().eq("spu_id", spuId));
+    }
+
+    @Override
+    public void updateSpuAttr(Long spuId, List<PmsSpuAttrValue> entities) {
+        //1、删除这个spuId之前对应的所有属性
+        this.baseMapper.delete(new QueryWrapper<PmsSpuAttrValue>().eq("spu_id",spuId));
+        List<PmsSpuAttrValue> collect = entities.stream().peek(item -> item.setSpuId(spuId)).collect(Collectors.toList());
+        this.saveBatch(collect);
     }
 
 }
