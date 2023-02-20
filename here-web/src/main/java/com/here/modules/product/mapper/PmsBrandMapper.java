9caad74ffc02a0827913d5afd350ae4cc27600d2
banjiu00 <527134765@qq.com>
2023-02-20 14:36:38 +0800

    修改品牌管理

--- here-web/src/main/java/com/here/modules/product/mapper/PmsBrandMapper.java
+++ here-web/src/main/java/com/here/modules/product/mapper/PmsBrandMapper.java
@@ -3,6 +3,9 @@ package com.here.modules.product.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.here.modules.product.entity.PmsBrand;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 商品品牌管理mapper
@@ -12,5 +15,15 @@ import org.apache.ibatis.annotations.Mapper;
  */
 @Mapper
 public interface PmsBrandMapper extends BaseMapper<PmsBrand> {
+
+    /**
+     * 修改显示状态
+     */
+    int updateShowStatus(@Param("ids") List<Long> ids, @Param("status") Integer showStatus);
+
+    /**
+     * 修改厂家制造商状态
+     */
+    int updateFactoryStatus(@Param("ids") List<Long> ids, @Param("status") Integer factoryStatus);
 
 }
--- here-web/src/main/java/com/here/modules/product/service/PmsBrandService.java
+++ here-web/src/main/java/com/here/modules/product/service/PmsBrandService.java
@@ -2,7 +2,11 @@ package com.here.modules.product.service;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.here.modules.product.dto.PmsBrandParam;
 import com.here.modules.product.entity.PmsBrand;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * 商品品牌管理Service
@@ -11,8 +15,32 @@ import com.here.modules.product.entity.PmsBrand;
  * @since 2023/1/30 21:53
  */
 public interface PmsBrandService extends IService<PmsBrand> {
+
+    /**
+     * 创建品牌
+     */
+    int createBrand(PmsBrandParam pmsBrandParam);
+
+    /**
+     * 修改品牌
+     */
+    @Transactional
+    int updateBrand(Long id, PmsBrandParam pmsBrandParam);
+
+
     /**
      * 分页查询品牌
      */
     Page<PmsBrand> listBrand(String keyword, Integer showStatus, int pageNum, int pageSize);
+
+
+    /**
+     * 修改显示状态
+     */
+    int updateShowStatus(List<Long> ids, Integer showStatus);
+
+    /**
+     * 修改厂家制造商状态
+     */
+    int updateFactoryStatus(List<Long> ids, Integer factoryStatus);
 }
