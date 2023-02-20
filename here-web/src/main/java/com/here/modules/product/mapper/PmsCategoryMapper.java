0827d7bca18af8411e065e144987bc347661a9ae
banjiu00 <527134765@qq.com>
2023-02-20 14:40:53 +0800

    修改分类管理

--- here-web/src/main/java/com/here/modules/product/mapper/PmsCategoryMapper.java
+++ here-web/src/main/java/com/here/modules/product/mapper/PmsCategoryMapper.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.here.modules.product.dto.PmsCategoryWithChildrenItem;
 import com.here.modules.product.entity.PmsCategory;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -20,4 +21,15 @@ public interface PmsCategoryMapper extends BaseMapper<PmsCategory> {
      * 获取商品分类及其子分类
      */
     List<PmsCategoryWithChildrenItem> listWithChildren();
+
+
+    /**
+     * 批量修改导航状态
+     */
+    int updateNavStatus(@Param("ids") List<Long> ids, @Param("status") Integer navStatus);
+
+    /**
+     * 批量修改显示状态
+     */
+    int updateShowStatus(@Param("ids") List<Long> ids, @Param("status") Integer showStatus);
 }
